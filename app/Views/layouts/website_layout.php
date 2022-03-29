2f06121beb26bf61275b2562f76bdf7768962ae2
Aurangzaib Najam <aurangzaib.n@allshorestaffing.com>
2022-03-30 01:15:35 +0500

    i

--- app/Views/layouts/website_layout.php
+++ app/Views/layouts/website_layout.php
@@ -4,10 +4,15 @@
 <head>
     <title><?= esc($title) ?>
     </title>
+    <?= link_tag('css/website/main.css') ?>
 </head>
 
 <body>
-    <?= $this->renderSection('contents') ?>
+    <div class="dv-base">
+        <div class="dv-top">
+            <?= $this->renderSection('contents') ?>
+        </div>
+    </div>
 </body>
 
 </html>
