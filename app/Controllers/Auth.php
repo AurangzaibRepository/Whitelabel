03d2cc0a61711704caf4f53d2933cea6a8611c54
Aurangzaib Najam <aurangzaib.n@allshorestaffing.com>
2022-03-30 02:13:24 +0500

    Renames auth controller

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
-class Authentication extends BaseController
+class Auth extends BaseController
 {
     public function index()
     {
