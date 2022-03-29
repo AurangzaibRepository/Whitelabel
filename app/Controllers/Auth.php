8c8870c4931da0066d309b0223a6c541b1e1d541
Aurangzaib Najam <aurangzaib.n@allshorestaffing.com>
2022-03-30 02:15:00 +0500

    Loads html helper

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -6,6 +6,11 @@ use App\Controllers\BaseController;
 
 class Auth extends BaseController
 {
+    public function __construct()
+    {
+        helper('html');
+    }
+
     public function index()
     {
         $data['title'] = 'Login';
