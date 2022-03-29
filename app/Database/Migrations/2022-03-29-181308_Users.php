c2ed1a4bbf3b9bd3643ad7fd250c5ec7fd2211ee
Aurangzaib Najam <aurangzaib.n@allshorestaffing.com>
2022-03-30 00:28:17 +0500

    Adds filed in user migrations

--- app/Database/Migrations/2022-03-29-181308_Users.php
+++ app/Database/Migrations/2022-03-29-181308_Users.php
@@ -8,11 +8,40 @@ class Users extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'first_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50'
+            ],
+            'last_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50'
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '80',
+                'null' => true
+            ],
+            'role' => [
+                'type' => 'ENUM("admin", "user")',
+                'default' => 'user',
+                'null' => false
+            ],
+            'created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP',
+            'updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP'
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('users');
     }
 }
