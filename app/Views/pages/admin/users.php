34dd144b103b44376875b8d5501f77ab9451b5ce
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-31 02:20:41 +0500

    Creates admin users view

---
+++ app/Views/pages/admin/users.php
@@ -0,0 +1,10 @@
+<?= $this->extend('layouts/admin_layout') ?>
+
+<?= $this->section('contents') ?>
+<style>
+    .lnk-users {
+        background: #f2f2f2;
+        border-left: 2px solid #202b57;
+    }
+</style>
+<?= $this->endSection();
