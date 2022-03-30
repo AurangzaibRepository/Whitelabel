93dc2dd74370cde47e919088caf5145d858a5485
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-30 20:43:50 +0500

    Hash password

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,11 +11,12 @@ class UserModel extends Model
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $allowedFields = ['first_name', 'last_name', 'email', 'password'];
-    protected $request;
 
     public function saveRecord(RequestInterface $request): void
     {
-        $this->insert($request->getRawInput());
-    }
+        $data = $request->getRawInput();
+        $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
 
+        $this->insert($data);
+    }
 }
