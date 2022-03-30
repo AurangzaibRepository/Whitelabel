e9903d4d6e6431fa265e03093b470f144b391a00
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-30 19:46:01 +0500

    creates user model

---
+++ app/Models/UserModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table            = 'users';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+
+}
