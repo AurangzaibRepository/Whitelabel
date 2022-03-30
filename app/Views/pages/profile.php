b20c6ac04f6d1cdc8db698f5f203675ea4c6528d
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-30 22:45:42 +0500

    Creates profile view

---
+++ app/Views/pages/profile.php
@@ -0,0 +1,11 @@
+<?= $this->extend('layouts/website_layout') ?>
+
+<?= $this->section('contents') ?>
+<style>
+    .lnk-profile {
+        background: #f2f2f2;
+        border-left: 2px solid #202b57;
+    }
+</style>
+
+<?= $this->endSection() ?>
