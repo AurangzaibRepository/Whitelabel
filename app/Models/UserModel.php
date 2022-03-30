637fae341db899b625af5d816e29be3da94cb630
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-30 20:31:57 +0500

    Adds function to save data

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,17 +3,19 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use CodeIgniter\HTTP\RequestInterface;
 
 class UserModel extends Model
 {
     protected $table            = 'users';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
+    protected $allowedFields = ['first_name', 'last_name', 'email', 'password'];
+    protected $request;
 
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
+    public function saveRecord(RequestInterface $request): void
+    {
+        $this->insert($request->getRawInput());
+    }
 
 }
