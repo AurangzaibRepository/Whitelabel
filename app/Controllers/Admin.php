cb34e3bb97b593118db85aa2d28c7b182cdc05d6
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-31 01:47:31 +0500

    Implements login function

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -3,12 +3,16 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class Admin extends BaseController
 {
+    private $model;
+
     public function __construct()
     {
         helper(['html', 'form']);
+        $this->model = model(UserModel::class);
     }
 
     public function index()
@@ -16,5 +20,23 @@ class Admin extends BaseController
         $data['title'] = 'Login';
 
         return view('pages/admin/login', $data);
+    }
+
+    public function login() 
+    {
+        $data = $this->request->getRawInput();
+
+        $user = $this  
+                    ->model
+                    ->where('email', $data['email'])
+                    ->where('role', 'admin')
+                    ->first();
+
+        if (!empty($user) && password_verify($data['password'], $user['password'])) {
+            
+        }
+
+        session()->setFlashdata('error', 'Invalid credentials');
+        return redirect()->back();
     }
 }
