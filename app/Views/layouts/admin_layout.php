67f28cbdbde60287eb930f734fdb4ffcf2c62281
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-31 02:19:58 +0500

    Creates admin layout

---
+++ app/Views/layouts/admin_layout.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+
+<head>
+    <title><?= esc($title) ?>
+    </title>
+    <?= link_tag('css/bootstrap.min.css') ?>
+    <?= link_tag('css/website/main.css') ?>
+
+    <!-- Scripts -->
+    <?= script_tag('js/jquery.min.js') ?>
+</head>
+
+<body>
+    <div class="dv-base">
+        <div class="dv-top">
+            <label>School Of Whales</label>
+        </div>
+        <div class="dv-layout">
+            <div class="dv-leftmenu">
+                <div class="dv-logo">
+                    <?= img('images/icon.svg') ?>
+                </div>
+
+                <?= view('layouts/admin_left_menu') ?>
+
+            </div>
+            <div class="dv-contents">
+                <?= $this->renderSection('contents') ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ app/Views/layouts/admin_left_menu.php
@@ -0,0 +1,8 @@
+<ul class="ul-left-menu">
+    <li>
+        <a href="/admin/users" class="lnk-users">Users</a>
+    </li>
+    <li>
+        <a href="/admin/logout">Logout</a>
+    </li>
+</ul>
