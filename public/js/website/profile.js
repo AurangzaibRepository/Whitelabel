6d3c859af083a6223659d2277dde9514591d708e
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-31 00:14:53 +0500

    Implements function to validate profile

--- public/js/website/profile.js
+++ public/js/website/profile.js
@@ -1,4 +1,22 @@
-$(function(){
+var isValid;
 
-    alert('asdas');
-})
+function validateForm() {
+    
+    isValid = true;
+    $('.spn-error').css('display', 'none');
+
+    validateField('first_name', 'error-fname');
+    validateField('last_name', 'error-lname');
+    validateField('email', 'error-email');
+
+    return isValid;
+}
+
+function validateField(elementID, errorID) {
+
+    if ($(`#${elementID}`).val().trim() === '') {
+
+        isValid = false;
+        $(`#${errorID}`).css('display', 'block');
+    }
+}
