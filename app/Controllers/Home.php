1f2d83675885bb00b3a53d4be8cccebdfa1a2e95
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-31 00:15:34 +0500

    Retrieves user data

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,16 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\UserMoel;
+
 class Home extends BaseController
 {
+    private $model;
+
     public function __construct()
     {
         helper(['html', 'form']);
+        $this->model = model(UserModel::class);
     }
 
     public function index()
     {
-        $data['title'] = 'Home';
+        $data = [
+            'title' => 'Home',
+            'data' => $this->model->find(session()->get('id'))
+        ];
 
         return view('pages/profile', $data);
     }
