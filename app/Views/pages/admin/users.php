69dda45798f008243d3a6cd493b7f1a097066211
Aurangzaib NAjam <aurangzaib.n@allshorestaffing.com>
2022-03-31 02:27:35 +0500

    Table layout

--- app/Views/pages/admin/users.php
+++ app/Views/pages/admin/users.php
@@ -7,4 +7,16 @@
         border-left: 2px solid #202b57;
     }
 </style>
+
+<table class="table table-bordered table-stripped">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+</table>
+
 <?= $this->endSection();
